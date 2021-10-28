a8ae66a0b970697991e0c0256085572d6e994382
Chase Wrenn <chasewrenn@ufl.edu>
2021-10-28 17:17:51 -0400

    Enlarge Pumpkin and Begin Mouse Listener

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,8 @@ package main;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
 
@@ -10,14 +12,42 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame("Jack-O-Lantern Carver");
+        frame.setPreferredSize(new Dimension(600, 600));
         JPanel imagePanel = (JPanel) frame.getContentPane();
         JImageComponent imageComponent = new JImageComponent();
         Image image = ImageIO.read(new File("src/main/Assets/PumpkinBackground.jpeg"));
         Icon icon = new ImageIcon(image);
         imageComponent.setIcon(icon);
+        imageComponent.setSizeMode(JImageComponent.SizeMode.STRETCH);
         imagePanel.add(imageComponent);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        MouseListener mouseListener = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        };
     }
 }
