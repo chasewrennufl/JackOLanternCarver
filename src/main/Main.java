539112f6748a651a96a87a3ea1aef2befca4a82d
Chase Wrenn <chasewrenn@ufl.edu>
2021-10-28 16:58:56 -0400

    Pumpkin Image Displays in Pane

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,21 @@
 package main;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame("Jack-O-Lantern Carver");
+        JPanel imagePanel = (JPanel) frame.getContentPane();
+        JImageComponent imageComponent = new JImageComponent();
+        Image image = ImageIO.read(new File("src/main/Assets/PumpkinBackground.jpeg"));
+        Icon icon = new ImageIcon(image);
+        imageComponent.setIcon(icon);
+        imagePanel.add(imageComponent);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
