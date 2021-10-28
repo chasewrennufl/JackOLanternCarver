4bb54e9e50499b1e99673aa46c2b971c17fbd1b9
Chase Wrenn <chasewrenn@ufl.edu>
2021-10-28 16:23:09 -0400

    JFrame Up and Working

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Jack-O-Lantern Carver");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
